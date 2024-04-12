8ff5ec0beffa393e43f21186becd8c00997dc9cc
havok2063 <havok2063@hotmail.com>
2024-04-12 13:31:25 -0400

    adding home component and creating routes for embed and dashboard

---
+++ sdss_solara/pages/home.py
@@ -0,0 +1,22 @@
+
+import solara
+
+from sdss_solara.pages.jdaviz_embed import Page as Embed
+from sdss_explorer.pages import Page as Dashboard, Layout as DashLayout
+
+@solara.component
+def Layout(children=[]):
+    # there will only be 1 child, which is the Page()
+    return children[0]
+
+
+@solara.component
+def Home():
+    solara.Markdown("SDSS Solara Home")
+
+
+routes = [
+    solara.Route(path="/", component=Home, label="Home", layout=Layout),
+    solara.Route(path="embed", component=Embed, label="Embed"),
+    solara.Route(path="dashboard", component=Dashboard, label="Dashboard", layout=DashLayout),
+]
